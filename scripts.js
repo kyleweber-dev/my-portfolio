f43a546d394209656a7db4dea55bf6645749fc42
Kyle <124207267+kyleweber-dev@users.noreply.github.com>
2025-02-11 20:42:22 -0700

    Updates

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// document.addEventListener("mousemove", (event) => {
+//   const cursor = document.querySelector(".cursor");
+
+//   // Move the cursor effect to the mouse position
+//   cursor.style.left = `${event.clientX}px`;
+//   cursor.style.top = `${event.clientY}px`;
+
+//   // Optional: Slight scale effect for smoothness
+//   cursor.style.transform = `translate(-50%, -50%) scale(1.1)`;
+// });
